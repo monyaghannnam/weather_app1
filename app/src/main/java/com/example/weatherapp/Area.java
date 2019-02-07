9a72b53d24ba7bc62a1a1f0dc0e99469b9da7c2e
monyaghannnam <monya.jamal.96@gmail.com>
2019-02-07 21:56:49 +0200

    Action Bar

---
+++ app/src/main/java/com/example/weatherapp/Area.java
@@ -0,0 +1,27 @@
+package com.example.weatherapp;
+
+public class Area {
+    String name;
+    int id;
+
+    public Area(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
